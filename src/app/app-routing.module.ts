fdea8ada505205d911b88fcfc862e7bb49d0eefa
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-02-15 15:19:01 -0300

    v0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './Components/carrito/carrito.component';
+import { DetallesProductoComponent } from './Components/Detalles/detalles-producto/detalles-producto.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"detalles/:id", component: DetallesProductoComponent},
+  {path:"carrito", component: CarritoComponent},
+  {path:"**", redirectTo: "home"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/statics/navbar/navbar.component';
+import { CardComponent } from './Components/cards/card-producto/card.component';
+import { LoginComponent } from './Components/statics/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CategoriaComponent } from './Components/cards/card-categoria/categoria.component';
+import { FooterComponent } from './Components/statics/footer/footer.component';
+import { CarritoComponent } from './Components/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    LoginComponent,
+    HomeComponent,
+    CategoriaComponent,
+    FooterComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
