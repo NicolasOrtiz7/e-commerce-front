a61093d2fed4222958e22b16b734cd96758ec35b
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-02-15 15:50:56 -0300

    se agrega ruta temporal para visualizar categorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarritoComponent } from './Components/carrito/carrito.component';
 import { DetallesProductoComponent } from './Components/Detalles/detalles-producto/detalles-producto.component';
 import { HomeComponent } from './Components/home/home.component';
+import { ProductosComponent } from './Components/productos/productos.component';
 
 const routes: Routes = [
   {path:"home", component: HomeComponent},
   {path:"detalles/:id", component: DetallesProductoComponent},
   {path:"carrito", component: CarritoComponent},
+  {path:"temporal", component: ProductosComponent},
   {path:"**", redirectTo: "home"},
 ];
 
