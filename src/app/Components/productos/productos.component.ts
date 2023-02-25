3f9d5de657157a3fbabda8c6409402d568d52d72
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-02-24 22:49:30 -0300

    cantidad de productos por categoria

--- src/app/Components/productos/productos.component.ts
+++ src/app/Components/productos/productos.component.ts
@@ -13,7 +13,16 @@ export class ProductosComponent implements OnInit{
   
   @Input() allProductos:Producto;
 
+  remeras:number = 0;
+  pantalones:number = 0;
+  camperas:number = 0;
+  buzos:number = 0;
+  calzado:number = 0;
+  gorros:number = 0;
+
+  productos: Producto[];
   productoCategoria: any;
+  cantidad: number;
 
   constructor(
     private router:Router,
@@ -22,16 +31,38 @@ export class ProductosComponent implements OnInit{
     ){}
 
   ngOnInit(): void {
+    this.listAllProductos();
     this.listProductoCategoria("remeras");
+  }
+
+  // Para cargar el número de prendas que hay en cada categoría
+  listAllProductos(){
+    this.productoService.listAllProductos().subscribe(
+      data => {
+        this.productos = data;
+
+        this.productos.forEach((item:any) => {
+
+          let a = item.categoria.nombre
+          if (a == "remeras") this.remeras++
+          if (a == "pantalones") this.pantalones++
+          if (a == "camperas") this.camperas++
+          if (a == "calzado") this.calzado++
+          if (a == "buzos") this.buzos++
+          if (a == "gorros") this.gorros++
+
+        });
+      },
+      err => {console.log(err)}
+    )
   }
   
   listProductoCategoria(categoria:string){
     this.productoService.listProductoCategoria(categoria).subscribe(
       data => {
         this.productoCategoria = data;
-        console.log(data)
-        console.log(this.productoCategoria)
-      }
+      },
+      err => { console.log(err) }
     )
   }
 
